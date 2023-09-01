20156581dd5fe20f85419487a5a6bce29c983723
Gesma94 <46707463+Gesma94@users.noreply.github.com>
2023-09-01 18:47:49 +0200

    F!! Add nopath API

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import express = require("express");
 
 export const app = express();
 
+app.get('/', (request, response) => {
+    response.sendStatus(200);
+});
+
 app.get('/ping', (request, response) => {
     console.log("[REQUEST] '/ping'");
     response.status(200).send("Server is running ✅");
